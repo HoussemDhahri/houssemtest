5086bdbc9740529d6573713da5bc4e58fdcfa98b
HoussemDhahri <98754807+HoussemDhahri@users.noreply.github.com>
2022-10-27 01:37:57 +0100

    Add files via upload

---
+++ houssemtest.php
@@ -0,0 +1,20 @@
+<body>
+
+    <?php
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+    $houssemnamebook=$_POST["bookname"];
+	echo "<br>";
+	$houssemlien1=$_POST["direct"];
+	echo "<br>";
+	$houssemlien2=$_POST["mediafire"];
+	echo "<br>";	
+	$houssemlien3=$_POST["mega"];
+	echo "<br>";
+    echo "<br>";
+    echo "<br>";	
+	
+	
+	
+	?>
